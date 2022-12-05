425832eb98f06b93d29010f7a2363aeee3516599
Darío Carrillo <dario.carrillo6311@alumnos.udg.mx>
2022-12-04 23:48:43 -0600

    backup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,92 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="img.cont">
+          <img alt="Vue logo" src="../assets/img/logo-qci-border.svg">
+          <h2 class="text-center text-lg-start">
+            Conéctate con amigos y el resto del mundo usando qcibook</h2>
+        </div>
+      </div>
+      <div class="col-lg-6">
+          <card-comp :btns="btnArray" class="mx-auto card-comp">
+            <form>
+              <div class="mb-3 mt-2">
+              <input type="email" class="form-control" placeholder="Correo electrónico">
+              </div>
+              <div class="mb-3 mt-2">
+              <input type="password" class="form-control" placeholder="Contraseña">
+              </div>
+            </form>
+          </card-comp>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import CardComp from '@/components/CardComp.vue'
+  export default {
+    name: 'HomeView',
+    components: {
+      CardComp,
+    },
+  
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+    data(){
+      return {
+        btnArray:[
+          {
+          txt: 'Iniciar sesión',
+          class: 'btn-primary',
+          },
+          {
+          txt: 'Borrar',
+          class: 'btn-danger',
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+
+
+<style scoped>
+  .home {
+    max-width: 895px;
+    margin: auto;
+  }
+
+  .img-cont {
+    display: flex;
+    flex-direction: column;
+  }
+
+img {
+  max-width: 315px;
+  margin: 1em auto 0 auto;
+}
+
+  .card-comp{
+    max-width: 398px;
+    margin-top: 2.6em;
+  }
+
+@media (min-width: 992px){
+  .home {
+    margin: 4em auto;
+  }
+  img {
+    margin: 0;
+    margin-top: 5.6em;
+  }
+  h2 {
+    margin: 0;
+    margin-top: -0.10em;
+    margin-left: 0.8em;
   }
 }
-</script>
+
+</style>
